f0bdd8008345d67f5ac6d3b19d9162c38db06048
SpeedStream <aesc91@hotmail.com>
2016-05-05 04:05:16 -0500

    last

--- realizarCompra.php
+++ realizarCompra.php
@@ -7,8 +7,10 @@
 </head>
 <body>
 <?php
-   $key = $_GET['comprar'];
-   $usr=mysql_query("SELECT NumViajes FROM Usuario WHERE idUsuario='".$key."'");
+	$conexion=mysql_connect("localhost","root","qwerty",false);
+	$bd = mysql_select_db("etseitSystem",$conexion);
+   	$key = $_GET['comprar'];
+   	$usr=mysql_query("SELECT NumViajes FROM Usuario WHERE idUsuario='".$key."'");
 	if(is_resource($usr)){
 		if(mysql_num_rows($usr)>0){
 			$viajes=mysql_fetch_array($usr);
@@ -19,6 +21,7 @@
 				echo $nuevosViajesRealizados;
 				echo $key;
 	   			$sql2 = "UPDATE Usuario SET NumViajes=".$nuevosViajesRealizados." WHERE idUsuario=".$key." ";
+	   			$result2 = mysql_query($sql2,$conexion);
 	   			if($sql2){
 				 	$sql=mysql_query("DELETE FROM ReservaAuto  WHERE fk_idUsuario_ReservaAuto='".$key."'");
 					$sql=mysql_query("DELETE FROM ReservaHotel WHERE fk_idUsuario_ReservaHotel='".$key."'");
